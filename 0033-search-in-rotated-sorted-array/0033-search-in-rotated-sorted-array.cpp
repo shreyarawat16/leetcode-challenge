d8659fa4385be958e3d9d4eb4ede7999ea81051a
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2023-10-28 20:23:52 +0530

    Time: 0 ms (100%), Space: 11.4 MB (24.21%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ind=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(target== nums[i]){
+                ind=i;
+            break;
+            }
+        }
+        return ind;
+    }
+};
