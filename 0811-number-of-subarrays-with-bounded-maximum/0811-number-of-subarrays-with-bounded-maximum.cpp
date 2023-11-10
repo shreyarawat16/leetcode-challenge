812a23f574b50c2ddc1f546744be4ccd61cee3f9
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2023-11-10 23:49:04 +0530

    Time: 39 ms (94.64%), Space: 52.4 MB (94.23%) - LeetHub

---
+++ 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int n =  nums.size();
+        int si =0;//starting index
+        int ei = 0; //ending index
+        int result =0, prev_count =0;
+        while(ei < n){
+            if(left <= nums[ei]  &&  nums[ei]<= right){
+              prev_count = ei- si +1;
+              result += prev_count;
+            }else if (  nums[ei] < left){
+                 result += prev_count;
+            }else{
+                //right < nums[ei]
+                si = ei +1;
+                prev_count =0;
+            }
+            ei++;
+       }
+      return result;      
+    }
+};
