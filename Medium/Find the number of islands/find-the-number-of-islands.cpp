6fffd419ff105e9e8f19706ae841d7189fc91ffd
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2024-02-14 16:41:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -5,45 +5,45 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  void bfs(int row, int col, vector<vector<char>> grid, vector<vector<int>>& vis){
-    vis[row][col]=1;
-    queue<pair<int, int>> q;
-    q.push({row,col});
-    int n=grid.size();
-    int m=grid[0].size();
-    while(!q.empty()){
-   int r= q.front().first;
-   int c= q.front().second;
-   q.pop();
-   for(int deltarow=-1; deltarow<=1; deltarow++){
-       for(int deltacol=-1; deltacol<=1; deltacol++){
-          int nrow= r+deltarow;
-        int ncol=c+deltacol;
-           if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
-               vis[nrow][ncol]=1;
-               q.push({nrow, ncol});
-           }
-       }
-   }
-    }
-}
+  void bfs(int row, int col, vector<vector<char>> grid, vector<vector<int>>& vis, int n, int m)
     // Function to find the number of islands.
-    int numIslands(vector<vector<char>>& grid) {
-        // Code here
-         int n= grid.size();
-        int m= grid[0].size();
-        int cnt=0;
-        vector<vector<int>> visited(n, vector<int>(m,0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                      cnt++;
-                    bfs(i,j,grid,visited);
-                  
+    {
+        queue<pair<int,int>> q;
+        vis[row][col]=1;
+        q.push({row,col});
+        
+        
+        while(!q.empty()){
+            int r= q.front().first;
+            int c= q.front().second;
+            q.pop();
+            for(int i=-1; i<=1; i++){
+                for(int j=-1; j<=1; j++){
+                    int nr= r+i;
+                    int nc= c+j;
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]=='1'){
+                        vis[nr][nc]=1;
+                        q.push({nr, nc});
+                        
+                    }
                 }
             }
         }
-        return cnt;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int ans=0;
+        int n= grid.size(), m=grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    ans++;
+                    bfs(i,j,grid,vis,n,m);
+                }
+            }
+        }
+        return ans;
     }
 };
 
