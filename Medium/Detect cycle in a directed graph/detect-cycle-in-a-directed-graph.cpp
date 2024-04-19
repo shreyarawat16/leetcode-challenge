e9cb4d0972e23a63de26b0a53da40c8e61aed463
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2024-04-19 16:41:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -6,69 +6,36 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    /*
-    bool CheckDfs(int node, vector<int> adj[],int vis[],int pvis[]){
-        vis[node] = 1;
-        pvis[node]=1;
-        
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                if(CheckDfs(it,adj,vis,pvis)==true)
-                return true;
-            }
-             else if(pvis[it]==true)
-                 return true;
-           
-        }
-        pvis[node]=0;
-        return false;
-    }
-    */
-     
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        /*
-        int vis[V]={0};
-        int pvis[V]={0};
-        
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-               if(CheckDfs(i,adj,vis,pvis)==true)
-               return true;
-              
-            }
-        }
-        return false;
-        */
-        int indegree[V]= {0};
-        for(int i=0; i<V;i++){
+        int indegree[V]={0};
+        for(int i=0; i<V; i++){
             for(auto it: adj[i]){
-            indegree[it]++;
-        }
+                indegree[it]++;
+            }
         }
         queue<int> q;
         for(int i=0; i<V; i++){
             if(indegree[i]==0){
                 q.push(i);
             }
-            
         }
         vector<int> ans;
         while(!q.empty()){
             int node= q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto it: adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
-            }
+             q.pop();
+             ans.push_back(node);
+             for(auto it: adj[node]){
+                 indegree[it]--;
+                 if(indegree[it]==0){
+                     q.push(it);
+                 }
+             }
         }
-        if(ans.size()<V){
-            return true;
+        if(ans.size()==V){
+            return false;
         }
-        return false;
+        return true;
     }
 };
 
