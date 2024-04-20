3b644f736cdbd1b1f2feef3e444e86678cfc46a6
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2024-04-20 12:39:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
+++ Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -12,35 +12,36 @@ class Solution {
   public:
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> destination) {
-        // code here
-        int n= grid.size();
-        int m= grid[0].size();
-        int delrow[]={-1,0,+1,0};
-        int delcol[]= {0,+1,0,-1};
+        int n= grid.size(), m=grid[0].size();
         queue<pair<int, pair<int, int>>> q;
+        
+        q.push({0, {source.first, source.second}});
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        dist[source.first][source.second]= 0;
-        q.push({0,{source.first, source.second}});
+        
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+        dist[source.first][source.second]=0;
         while(!q.empty()){
-            auto it= q.front();
+            int row= q.front().second.first;
+            int col= q.front().second.second;
+            int d= q.front().first;
              q.pop();
-           int d= it.first;
-           int r= it.second.first;
-           int c= it.second.second;
-          if(r== destination.first && c== destination.second){
-              return dist[r][c];
-          }
-           for(int i=0; i<4; i++){
-               int nr= r+ delrow[i];
-               int nc= c+ delcol[i];
-               if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && d+1< dist[nr][nc]){
-                  
-                       dist[nr][nc]= d+1;
-                       
-                       q.push({d+1, {nr, nc}});
-                   }
-               }
-           }
+            if(row== destination.first && col== destination.second){
+                return d;
+            }
+            
+           
+            for(int i=0; i<4; i++){
+                int nr= row+ dr[i];
+                int nc= col+ dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                    if(d+1 < dist[nr][nc]){
+                        dist[nr][nc]= d+1;
+                        q.push({d+1, {nr,nc}});
+                    }
+                }
+            }
+        }  
         
         return -1;
     }
