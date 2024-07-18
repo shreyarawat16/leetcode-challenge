adf1c3eabd65836900f493c09dc7306d250254c2
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2024-07-18 20:37:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -13,32 +13,34 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-         stack<int> st;
-         for(int i=n-1; i>=0; i--){
-             st.push(i);
-         }
-         while(st.size()>1){
-             int x= st.top(); st.pop();
-             int y= st.top(); st.pop();
-             if(M[x][y] && !M[y][x]){
-                 st.push(y);
-             }
-             else if(M[y][x] && !M[x][y]){
-                 st.push(x);
-             }
-         }
-         if(st.empty()) return -1;
-         int num= st.top();
-         int row=0, col=0;
-         for(int i=0; i<n; i++){
-             row+= M[num][i];
-             col+= M[i][num];
-             
-         }
-         if(row==0 && col==n-1){
-             return num;
-         }
-         return -1;
+        // code here 
+       stack<int> st;
+       for(int i=n-1; i>=0; i--){
+           st.push(i);
+       }
+       while(st.size()>1){
+           int x= st.top(); st.pop();
+           int y= st.top(); st.pop();
+           if(M[x][y] && !M[y][x]){
+               st.push(y);
+           }
+           else if(M[y][x] && !M[x][y]){
+               st.push(x);
+           }
+       }
+       if(st.empty()){
+           return -1;
+       }
+       int row=0, col=0;
+       int ind= st.top();
+       for(int i=0; i<n; i++){
+           row+= M[ind][i];
+           col+= M[i][ind];
+       }
+       if(row==0 && col==n-1){
+           return st.top();
+       }
+       return -1;
     }
 };
 
