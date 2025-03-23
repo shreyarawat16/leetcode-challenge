39fe329554c0e0d4cc3d1aa0112c91e6701174ec
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2025-03-23 13:07:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -4,60 +4,53 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    // Function to find floor of x
-    // n: size of vector
-    // x: element whose floor is to find
-    int findFloor(vector<long long> v, int n, long long x) {
 
+    int findFloor(vector<int>& arr, int x) {
         // Your code here
-        int start=0, end= n-1;
-        int ans=-1;
-        while( start<= end){
-            int mid= start+ (end-start)/2;
-            if(x == v[mid]){
-                ans= mid;
-                break;
-            }
-            else if(x > v[mid]){
-                start= mid+1;
-                ans= mid;
+        int n= arr.size(), ind=-1;
+        int low=0, high= n-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(arr[mid]<= x){
+                ind= mid;
+                low= mid+1;
             }
             else{
-                end= mid-1;
+                high= mid-1;
             }
-            
         }
-        return ans;
+        return ind;
     }
 };
 
 
+
 //{ Driver Code Starts.
-
 int main() {
-
-    long long t;
-    cin >> t;
-
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
     while (t--) {
-        long long n;
-        cin >> n;
-        long long x;
-        cin >> x;
 
-        vector<long long> v;
-
-        for (long long i = 0; i < n; i++) {
-            long long temp;
-            cin >> temp;
-            v.push_back(temp);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
-        Solution obj;
-        cout << obj.findFloor(v, n, x) << endl;
-    }
+        string ks;
+        getline(cin, ks);
+        int x = stoi(ks);
+        Solution ob;
+        int ans = ob.findFloor(arr, x);
 
+        cout << ans << endl;
+    }
     return 0;
 }
 // } Driver Code Ends
