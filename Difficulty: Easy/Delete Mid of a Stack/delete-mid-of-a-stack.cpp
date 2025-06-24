c7dfa70a326cac1101315dbb761f682a0b8fc4aa
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2025-06-24 11:03:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
@@ -1,66 +1,24 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution
-{
-    public:
-    //Function to delete middle element of a stack.
-    void deleteMid(stack<int>&s, int sizeOfStack)
-    {
-        // code here.
-        stack<int> s2; 
-        int n= (sizeOfStack)/2;
-        int i=0;
-        while(i<n){
-            int val= s.top();
-            s.pop();
-            i++;
-            s2.push(val);
-        }
-        int mid= s.top();
-        s.pop();
-        while(!s2.empty()){
-            s.push(s2.top());
-            s2.pop();
-        }
+class Solution {
+  public:
+  void solve(stack<int>& st, int mid, int index){
+      if(index==mid){
+          st.pop();
+          return;
+      }
+      int val =st.top();
+      st.pop();
+      index++;
+      solve(st, mid, index);
+      st.push(val);
+      return;
+  }
+    // Function to delete middle element of a stack.
+    void deleteMid(stack<int>& s) {
+        // code here..
+        int n= s.size();
+        int mid= n/2 ;
+        solve(s, mid, 0);
+        return;
+        
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin>>t;
-    
-    while(t--)
-    {
-        int sizeOfStack;
-        cin>>sizeOfStack;
-        
-        stack<int> myStack;
-        
-        for(int i=0;i<sizeOfStack;i++)
-        {
-            int x;
-            cin>>x;
-            myStack.push(x);    
-        }
-
-            Solution ob;
-            ob.deleteMid(myStack,myStack.size());
-            while(!myStack.empty())
-            {
-                cout<<myStack.top()<<" ";
-                myStack.pop();
-            }
-        cout<<endl;
-    }   
-    return 0;
-}
-
-// } Driver Code Ends
