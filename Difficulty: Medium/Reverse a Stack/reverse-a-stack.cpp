371b5c58fd61649ab528a41c54e3ab7c3364a0fb
shreyarawat16 <139633975+shreyarawat16@users.noreply.github.com>
2025-06-24 11:38:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -1,67 +1,28 @@
-//{ Driver Code Starts
-//Initial Template for C++
+// User function Template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-void insertAtBottom(stack<int>& st, int val){
-    //base case
-    if(st.empty()){
-       st.push(val);
-       return;
-    }
-    int x= st.top();
-    st.pop();
-    insertAtBottom(st, val);
-    st.push(x);
-}
-    void Reverse(stack<int> &st){
-        if(st.empty()){
+class Solution {
+  public:
+  void insertAtBottom(stack<int>& st,  int x){
+      if(st.empty()){
+          st.push(x);
+          return;
+      }
+      int val= st.top();
+      st.pop();
+      insertAtBottom(st,x);
+      st.push(val);
+      return;
+  }
+    void Reverse(stack<int> &St) {
+        if(St.empty()){
+        
             return;
         }
-        int val= st.top();
-        st.pop();
-        Reverse(st);
-        insertAtBottom(st, val);
-        
-    }
+        int val= St.top();
+        St.pop();
+        Reverse(St);
+        insertAtBottom(St, val);
+        return;
+   }
+   
 };
-
-//{ Driver Code Starts.
-
-
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        int N;
-        cin>>N;
-        stack<int> St;
-        for(int i=0;i<N;i++){
-            int x;
-            cin>>x;
-            St.push(x);
-        }
-        Solution ob;
-        ob.Reverse(St);
-        vector<int>res;
-        while(St.size())
-        {
-            res.push_back(St.top());
-            St.pop();
-        }
-        for(int i = res.size()-1;i>=0;i--)
-        {
-            cout<<res[i]<<" ";
-        }
-        
-        cout<<endl;
-    }
-}
-// } Driver Code Ends
