2cfea3a5d73c029a0b83a84ccff4a52c155dd6c8
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-06-25 14:14:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,64 +1,25 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-class Solution
-{
-    public:
-    //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
-    {
-       // Your code here
-       vector<int> ngeLeft(n,-1);
-       vector<int> ans(n,0);
-       stack<int> st;
-       for(int i=0; i<n; i++){
-           while(!st.empty() && price[st.top()]<= price[i]){
-               st.pop();
-           }
-           if(!st.empty()){
-               ngeLeft[i]= st.top();
-           }
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // write code here
+        int n= arr.size();
+        vector<int> ans(n,1);
+        stack<int> st;
+        for(int i=0; i<n; i++){
+          
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                
+                st.pop(); 
+            }
+            if(st.empty()){
+                ans[i]= i+1;
+            }
+            else{
+                ans[i]=i-st.top();
+            }
+           
            st.push(i);
-       }
-       for(int i=0; i<n; i++){
-          ans[i]= i-ngeLeft[i];
-       }
-       return ans;
+        }
+        return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int i,a[n];
-		for(i=0;i<n;i++)
-		{
-			cin>>a[i];
-		}
-		Solution obj;
-		vector <int> s = obj.calculateSpan(a, n);
-		
-		for(i=0;i<n;i++)
-		{
-			cout<<s[i]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
-}
-
-// } Driver Code Ends
