f2aba9b90675193055aa186b397e50331e2a6bfd
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-07-23 22:44:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -1,56 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-
     int findFloor(vector<int>& arr, int x) {
-        // Your code here
-        int n= arr.size(), ind=-1;
+        // code here
+        int ans=-1;
+        int n=arr.size();
         int low=0, high= n-1;
         while(low<=high){
-            int mid= (low+high)/2;
-            if(arr[mid]<= x){
-                ind= mid;
-                low= mid+1;
+            int mid= (high+low)/2;
+            if(arr[mid]<=x){
+                ans=mid;
+                low=mid+1;
             }
             else{
-                high= mid-1;
+                high=mid-1;
             }
         }
-        return ind;
+        return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int x = stoi(ks);
-        Solution ob;
-        int ans = ob.findFloor(arr, x);
-
-        cout << ans << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
