3318d3fce8d90a867d43c9e6a3162cdc9f551d2b
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-08-27 13:19:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,23 +1,25 @@
 class Solution {
   public:
-  void DFS(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ans){
-      vis[node]=1;
-      ans.push_back(node);
-      for(auto i: adj[node]){
-          if(node==i) continue;
-          if(!vis[i]){
-              DFS(i, adj, vis, ans);
-          }
-      }
-  }
+ void DFS(int start, vector<vector<int>>& adj,vector<int>& vis, vector<int>& ans ){
+     vis[start]=1;
+     ans.push_back(start);
+     for(auto it: adj[start]){
+         if(it == start){
+             continue;
+         }
+         if(!vis[it]){
+             DFS(it, adj, vis, ans);
+         }
+     }
+ }
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
         vector<int> ans;
         int n= adj.size();
-       
-        
         vector<int> vis(n,0);
-        DFS(0,  adj, vis, ans);
+        
+             DFS(0,adj, vis, ans);
+      
         return ans;
     }
 };
