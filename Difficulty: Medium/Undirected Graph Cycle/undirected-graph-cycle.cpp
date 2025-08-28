bf8336c49a1294c18421569bf2b6e3eb095b479b
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-08-28 17:37:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,44 +1,35 @@
 class Solution {
   public:
-
-
-    bool isCycle(int V, vector<vector<int>> edges) {
-        // Code here
-       
-        vector<int> vis(V,0);
-       vector<vector<int>> adj(V);
-       for(int i=0; i<edges.size(); i++){
-          int u= edges[i][0];
-          int v= edges[i][1];
-          adj[u].push_back(v);
-          adj[v].push_back(u);
-       }
-       queue<pair<int,int>> q;
-       for(int i=0; i<V; i++){
-           if(!vis[i]){
-                vis[i]=1;
-               q.push({i,-1});
-           
-           while(!q.empty()){
-           int u= q.front().first;
-           int parent= q.front().second;
-           q.pop();
-           for(auto it: adj[u]){
-               if(!vis[it]){
-                   vis[it]=1;
-                   q.push({it,u});
-               }
-               else if(it!= parent){
-                   return true;
-               }
+  bool DFS(int start, vector<vector<int>>& adj, vector<int>& vis, int parent ){
+       vis[start]=1;
+       for(auto it : adj[start]){
+           if(!vis[it]){
+              if( DFS(it, adj, vis, start)==true){
+                  return true;
+              }
            }
-           
+           else if(it!= parent){
+               return true;
+           }
        }
-       }
-       }
-      
-       
-       
+       return false;
+  }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        int m= edges[0].size();
+        vector<vector<int>> adj(V);
+        vector<int> vis(V,0);
+        for(auto it: edges){
+            int u= it[0];
+            int v= it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+      for(int i=0; i<V; i++){
+         if(!vis[i] && DFS(i, adj, vis, -1)){
+        return true;
+      }
+}
         return false;
     }
 };
