90bbf7731ec7882f8582b2d834d50f49870c7d28
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-09-01 22:01:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,41 +1,62 @@
 class Solution {
   public:
-  bool DFS(int start,vector<vector<int>> &edges, int V, vector<int>& vis,vector<int>& pathVis ){
-      vis[start]=1;
-      pathVis[start]=1;
-      for(auto it: edges[start]){
+//   bool DFS(int start,vector<vector<int>> &edges, int V, vector<int>& vis,vector<int>& pathVis ){
+//       vis[start]=1;
+//       pathVis[start]=1;
+//       for(auto it: edges[start]){
          
-          if(!vis[it]){
-              if(DFS(it, edges, V, vis, pathVis)) {
-                  return true;
-              }
-          }
-          else if(pathVis[it]){
-              return true;
-          }
+//           if(!vis[it]){
+//               if(DFS(it, edges, V, vis, pathVis)) {
+//                   return true;
+//               }
+//           }
+//           else if(pathVis[it]){
+//               return true;
+//           }
         
-      }
-      pathVis[start]=0;
-      return false;
-  }
+//       }
+//       pathVis[start]=0;
+//       return false;
+//   }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
+        //USING TOP
         vector<int> vis(V,0);
         vector<int> pathVis(V,0);
         vector<vector<int>> adj(V);
+        queue<int> q;
         for(auto it: edges){
             int u= it[0];
             int v= it[1];
             adj[u].push_back(v);
             
         }
+        vector<int> indegree(V);
         for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(DFS(i, adj, V, vis,pathVis)== true){
-                    return true;
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
                 }
             }
         }
-        return false;
+      if(topo.size()==V){
+          return false;
+      }
+      return true;
     }
 };
