efd336f73f9bc378ca60058432190e3823d96a68
Shreya Rawat <139633975+shreyarawat16@users.noreply.github.com>
2025-11-10 00:17:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.cpp
@@ -1,22 +1,20 @@
 class Solution {
   public:
     // Function to delete middle element of a stack.
-    void solve(stack<int>& st, int count, int n){
-        if(count== n/2){
+    void solve(int cnt, stack<int>& st, int len){
+        if(cnt== len/2){
             st.pop();
             return;
         }
-        int val= st.top();
+        int num= st.top();
         st.pop();
-        count++;
-        solve(st, count, n);
-        st.push(val);
-        return;
+        solve(cnt+1, st,len);
+        st.push(num);
     }
     void deleteMid(stack<int>& s) {
         // code here..
+        int i=0;
         int n= s.size();
-        solve(s,0,n);
-        return;
+        solve(i, s,n);
     }
 };
